8ba7813507194fa1cc19d6970c581b28861216ad
frsiam <siam.frs@gmail.com>
2022-02-24 11:10:54 +0600

    get first 5 array of buddy

--- js/buddy.js
+++ js/buddy.js
@@ -5,6 +5,13 @@ const loadBuddies = () => {
 }
 
 const displayBuddies = x => {
-    console.log(x);
+    const buddies = x.results
+    const buddiesDiv = document.getElementById('buddies')
+    for(const buddy of buddies){
+        console.log(buddy.name.first, buddy.name.last);
+        const p = document.createElement('p')
+        p.innerText = buddy.email
+        buddiesDiv.appendChild(p)
+    }
 }
 loadBuddies()
