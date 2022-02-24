b858ad33cbc4bc8ec75d013c22be4b69612d0f5a
frsiam <siam.frs@gmail.com>
2022-02-24 12:36:35 +0600

    show the buddy title,name,email

--- js/buddy.js
+++ js/buddy.js
@@ -8,9 +8,10 @@ const displayBuddies = x => {
     const buddies = x.results
     const buddiesDiv = document.getElementById('buddies')
     for(const buddy of buddies){
-        console.log(buddy.name.first, buddy.name.last);
+        console.log(buddy.name.title);
         const p = document.createElement('p')
-        p.innerText = buddy.email
+        p.innerText = `Name: ${buddy.name.title} ${buddy.name.first} ${buddy.name.last}
+        Email: ${buddy.email}`
         buddiesDiv.appendChild(p)
     }
 }
